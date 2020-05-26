363ed8a1fdbe1b755a5a0a91143cd462c6dcc2f3
Charlotte Lui <charlotteytlui@gmail.com>
2020-05-26 14:56:15 +0300

    added score, and line remove when filled

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.querySelector('#start-button');
     const width = 10;
     let nextRandom = 0;
-    let timerId
+    let timerId;
+    let score = 0;
 
     //The Tetrominoes
     const lTetromino = [
@@ -94,6 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
             currentPosition = 4;
             draw();
             displayShape();
+            addScore();
         }
     };
 
@@ -167,8 +169,26 @@ document.addEventListener('DOMContentLoaded', () => {
             timerId = setInterval(moveDown, 1000);
             displayShape();
         }
+    });
 
-    })
+    // add score
+    function addScore() {
+        for (let i = 0; i < 199; i += width) {
+            const row = [i, i+1, i+2, i+3, i+4,i+5, i+6, i+7, i+8, i+9];
+            if (row.every( index => squares[index].classList.contains('taken'))) {
+                score += 10;
+                scoreDisplay.innerHTML = score;
+                row.forEach( index => {
+                    squares[index].classList.remove('taken');
+                    squares[index].classList.remove('tetromino');
+                });
+                const squaresRemoved = squares.splice(i, width);
+                squares = squaresRemoved.concat(squares);
+                squares.forEach( cell => grid.appendChild(cell));
+
+            }
+        }
+    }
 
 
 
